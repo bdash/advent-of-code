a5f16bc1b6225b96154617d8b50778e0b1dedcee
Mark Rowe <mrowe@bdash.net.nz>
2022-12-01 22:36:21 -0800

    Day 2 part 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -24,9 +24,9 @@ impl FromStr for Selection {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "A" | "X" => Ok(Self::Rock),
-            "B" | "Y" => Ok(Self::Paper),
-            "C" | "Z" => Ok(Self::Scissors),
+            "A" => Ok(Self::Rock),
+            "B" => Ok(Self::Paper),
+            "C" => Ok(Self::Scissors),
             _ => Err("Unknown selection".into()),
         }
     }
@@ -38,7 +38,21 @@ enum Winner {
     Draw,
 }
 
+impl FromStr for Winner {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(Self::Them),
+            "Y" => Ok(Self::Draw),
+            "Z" => Ok(Self::Us),
+            _ => Err("Unknown winner".into()),
+        }
+    }
+}
+
 impl Winner {
+    #[allow(unused)]
     fn new(them: Selection, us: Selection) -> Winner {
         use Selection::*;
 
@@ -49,6 +63,18 @@ impl Winner {
         }
     }
 
+    fn our_move(&self, them: Selection) -> Selection {
+        use Selection::*;
+        use Winner::*;
+
+        match (self, them) {
+            (Us, Scissors) | (Draw, Rock) | (Them, Paper) => Rock,
+            (Us, Paper) | (Draw, Scissors) | (Them, Rock) => Scissors,
+            _ => Paper,
+        }
+    }
+
+    #[allow(unused)]
     fn score(&self) -> u32 {
         match self {
             Self::Us => 6,
@@ -63,10 +89,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let score: u32 = input
         .split_ascii_whitespace()
-        .map(str::parse)
-        .map(Result::unwrap)
         .tuples()
-        .map(|(them, us)| Winner::new(them, us).score() + us.score())
+        .map(|(them, winner)| (them.parse().unwrap(), winner.parse::<Winner>().unwrap()))
+        .map(|(them, winner)| winner.score() + winner.our_move(them).score())
         .sum();
 
     println!("Score after all rounds was {score}");
